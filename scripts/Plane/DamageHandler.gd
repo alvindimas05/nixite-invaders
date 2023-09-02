06a1a57b8f8d92727a9e959ff7652ed0e0476aae
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2023-09-02 20:22:12 +0700

    feat: plane explosion

--- scripts/Plane/DamageHandler.gd
+++ scripts/Plane/DamageHandler.gd
@@ -1,17 +1,21 @@
 extends Area2D
 
 @export var effect_delay = .2
-@export var effect_opacity = .8
+@export var effect_color = Color(.7, .7, .7)
+
+var pre_explosion = preload("res://objects/plane_explosion.tscn")
 
 var parent: CharacterBody2D
 var sprite: Node2D
 var is_player: bool
 var root: Node
+var explosion: AnimatedSprite2D
 func _ready():
 	root = get_node("/root/Main")
 	parent = get_parent()
 	sprite = parent.get_node("Sprite2D")
 	is_player = parent.name == "Player"
+	explosion = pre_explosion.instantiate()
 	set_sound_destroy()
 
 # Send damage to parent (Character)
@@ -23,6 +27,15 @@ func send_damage(damage: int):
 # Destroy parent if hp below or equals to 0
 func check_for_destroy():
 	if parent.health_points > 0: return
+	destroy_plane()
+
+func destroy_plane():
+	var dupe: AnimatedSprite2D = explosion.duplicate()
+	dupe.position = parent.position
+	dupe.animation_finished.connect(func(): dupe.queue_free())
+	root.add_child(dupe)
+	dupe.play()
+	
 	parent.queue_free()
 	sound_destroy.play()
 
@@ -38,11 +51,11 @@ func effect():
 	timer.wait_time = effect_delay
 	# Return opacity effect
 	timer.timeout.connect(func():
-		sprite.modulate.a = 1
+		sprite.modulate = Color(1, 1, 1)
 		remove_child(timer)
 	)
 	
 	# Reduce opacity effect
-	sprite.modulate.a = effect_opacity
+	sprite.modulate = effect_color
 	add_child(timer)
 	timer.start()
