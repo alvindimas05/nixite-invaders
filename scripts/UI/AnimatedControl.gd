7effe7818f57e06700fd73dcb22ee606f8e23967
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2023-09-06 14:04:05 +0700

    feat: check if moving or showing

--- scripts/UI/AnimatedControl.gd
+++ scripts/UI/AnimatedControl.gd
@@ -7,9 +7,12 @@ extends Control
 
 var _show_position: Vector2
 var _hide_position: Vector2
+var on_move = false
+var on_show: bool
 
 func _ready():
 	reset_position()
+	on_show = !hide_on_default
 	if hide_on_default: position = _hide_position
 
 func reset_position():
@@ -18,11 +21,20 @@ func reset_position():
 
 func hide_position(): position = _hide_position
 
-func hide_ui(): _move_ui(_hide_position)
-func show_ui(): _move_ui(_show_position)
+func hide_ui():
+	_move_ui(_hide_position)
+	on_show = false
+func show_ui():
+	_move_ui(_show_position)
+	on_show = true
 
+var tween: Tween
 func _move_ui(ui_position: Vector2):
-	var tween = get_tree().create_tween()
+	if tween.is_running(): tween.stop()
+	
+	on_move = true
+	tween = get_tree().create_tween()
 	
 	tween.tween_property(self, "position", ui_position, animation_duration).set_trans(Tween.TRANS_BACK)
+	tween.tween_callback(func(): on_move = false)
 	tween.play()
